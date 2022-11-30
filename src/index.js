539254380f5bf8a87b7e8ca10caa734e2a79c50c
Alena Batushkova <alena.batushkova@gmail.com>
2022-11-30 22:42:26 +0300

    add alarm functionality

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-const alarmBtn = document.querySelector('#alarmButton');
 const wrapper = document.querySelector('.wrapper');
+const alarmBtn = document.querySelector('#alarmButton');
+const stopBtn = document.querySelector('#stopButton');
 const title = document.querySelector('#title');
 const selectList = document.querySelectorAll('[id^="select"]');
+const output = document.querySelector('#output');
+const alarmIcon = document.querySelector('.clock__icon');
+const rigntone = new Audio('./src/audio/alarm-clock.mp3');
+const selectMap = {
+  0: 'Hour',
+  1: 'Minute',
+  2: 'AM/PM'
+};
+let alarmTime = null;
 
 const setSelectOptions = () => {
   for (let i = 1; i <= 12; i++) {
@@ -23,34 +33,84 @@ const setSelectOptions = () => {
   }
 };
 
+const ringAlarm = () => {
+  rigntone.play();
+  rigntone.loop = true;
+  alarmIcon.classList.add('is-shaking');
+  stopBtn.classList.remove('hide');
+  alarmBtn.classList.add('hide');
+};
+
 const calculateTime = () => {
   const date = new Date();
-  let [hour, minute, second] = [
+  let [hh, mm, ss] = [
     date.getHours(),
     date.getMinutes(),
     date.getSeconds()
   ];
   let ampm = 'AM';
 
-  if (hour >= 12) {
-    hour -= 12;
+  if (hh >= 12) {
+    hh -= 12;
     ampm = 'PM';
   }
 
-  hour = hour === 0 ? '12' : hour;
-  hour = hour < 10 ? `0${hour}` : hour;
-  minute = minute < 10 ? `0${minute}` : minute;
-  second = second < 10 ? `0${second}` : second;
-  title.textContent = `${hour}:${minute}:${second} ${ampm}`;
+  hh = hh === 0 ? '12' : hh;
+  hh = hh < 10 ? `0${hh}` : hh;
+  mm = mm < 10 ? `0${mm}` : mm;
+  ss = ss < 10 ? `0${ss}` : ss;
+  title.textContent = `${hh}:${mm}:${ss} ${ampm}`;
+
+  if (alarmTime === `${hh}:${mm} ${ampm}`) {
+    ringAlarm();
+  }
 
   setTimeout(calculateTime, 1000);
 };
 
 const setAlarm = () => {
-  console.log('alarm set');
+  const regex = /(Hour|Minute|AM\/PM)/;
+  alarmTime = `${selectList[0].value}:${selectList[1].value} ${selectList[2].value}`;
+
+  if (regex.test(alarmTime)) {
+    output.textContent = 'Please, select a valid time to set Alarm!';
+    output.classList.replace('hide', 'error');
+    return;
+  }
+
+  selectList.forEach(s => s.setAttribute('disabled', true));
+  alarmBtn.textContent = 'Clear Alarm';
+  output.textContent = `Alarm set for ${alarmTime}!`;
+  output.classList.remove('hide', 'error');
 };
 
-alarmBtn.addEventListener('click', setAlarm);
+const clearAlarm = () => {
+  selectList.forEach((s, i) => {
+    s.removeAttribute('disabled');
+    s.value = selectMap[i];
+  });
+  alarmBtn.textContent = 'Set Alarm';
+  output.classList.add('hide');
+  output.textContent = '';
+}
+
+const toggleAlarm = (evt) => {
+  const targetContent = evt.target.textContent;
+
+  targetContent === 'Set Alarm' ? setAlarm() : clearAlarm();
+};
+
+const stopAlarm = () => {
+  alarmTime = null;
+  rigntone.pause();
+  alarmIcon.classList.remove('is-shaking');
+  clearAlarm();
+  alarmBtn.classList.remove('hide');
+  stopBtn.classList.add('hide');
+};
+
+alarmBtn.addEventListener('click', toggleAlarm);
+stopBtn.addEventListener('click', stopAlarm);
 
 calculateTime();
 setSelectOptions();
